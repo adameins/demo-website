923715e5ab2d8bb89a922c1d8c0bc405717808b6
adameins <55643721+adameins@users.noreply.github.com>
2023-01-04 23:00:10 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ export default function MyNavbar(){
 
   React.useEffect(() => {
     // Fetch the logo image from the API
-    fetch("restorankeluarga-solo.up.railway.app/api/profile/")
+    fetch("https://restorankeluarga-solo.up.railway.app/api/profile/")
       .then((response) => response.json())
       .then((data) => {
         setLogo(data[0].logo);
